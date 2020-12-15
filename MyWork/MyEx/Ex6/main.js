8d40ccda80830cc82d3f37e4d20f5290d107c459
長谷川　武大 <57701001+borubo@users.noreply.github.com>
2020-12-15 18:47:36 +0900

    Add files via upload

---
+++ MyWork/MyEx/Ex6/main.js
@@ -0,0 +1,288 @@
+'use strict';
+
+{
+  const F1 = document.getElementById('F1');
+  const closeF1 = document.getElementById('closeF1');
+  const modalF1 = document.getElementById('modalF1');
+  const maskF1 = document.getElementById('maskF1');
+
+  F1.addEventListener('click', () => {
+    modalF1.classList.remove('hiddenF1');
+    maskF1.classList.remove('hiddenF1');
+  });
+
+  closeF1.addEventListener('click', () => {
+    modalF1.classList.add('hiddenF1');
+    maskF1.classList.add('hiddenF1');
+  });
+
+  maskF1.addEventListener('click', () => {
+    closeF1.click();
+  });
+// ----------------------------------------------------------------
+  const F2 = document.getElementById('F2');
+  const closeF2 = document.getElementById('closeF2');
+  const modalF2 = document.getElementById('modalF2');
+  const maskF2 = document.getElementById('maskF2');
+
+  F2.addEventListener('click', () => {
+    modalF2.classList.remove('hiddenF2');
+    maskF2.classList.remove('hiddenF2');
+  });
+
+  closeF2.addEventListener('click', () => {
+    modalF2.classList.add('hiddenF2');
+    maskF2.classList.add('hiddenF2');
+  });
+
+  maskF2.addEventListener('click', () => {
+    closeF2.click();
+  });
+// ----------------------------------------------------------------
+  const F3 = document.getElementById('F3');
+  const closeF3 = document.getElementById('closeF3');
+  const modalF3 = document.getElementById('modalF3');
+  const maskF3 = document.getElementById('maskF3');
+
+  F3.addEventListener('click', () => {
+    modalF3.classList.remove('hiddenF3');
+    maskF3.classList.remove('hiddenF3');
+  });
+
+  closeF3.addEventListener('click', () => {
+    modalF3.classList.add('hiddenF3');
+    maskF3.classList.add('hiddenF3');
+  });
+
+  maskF3.addEventListener('click', () => {
+    closeF3.click();
+  });
+// ----------------------------------------------------------------
+  const F4 = document.getElementById('F4');
+  const closeF4 = document.getElementById('closeF4');
+  const modalF4 = document.getElementById('modalF4');
+  const maskF4 = document.getElementById('maskF4');
+
+  F4.addEventListener('click', () => {
+    modalF4.classList.remove('hiddenF4');
+    maskF4.classList.remove('hiddenF4');
+  });
+
+  closeF4.addEventListener('click', () => {
+    modalF4.classList.add('hiddenF4');
+    maskF4.classList.add('hiddenF4');
+  });
+
+  maskF4.addEventListener('click', () => {
+    closeF4.click();
+  });
+// ----------------------------------------------------------------
+  const F5 = document.getElementById('F5');
+  const closeF5 = document.getElementById('closeF5');
+  const modalF5 = document.getElementById('modalF5');
+  const maskF5 = document.getElementById('maskF5');
+
+  F5.addEventListener('click', () => {
+    modalF5.classList.remove('hiddenF5');
+    maskF5.classList.remove('hiddenF5');
+  });
+
+  closeF5.addEventListener('click', () => {
+    modalF5.classList.add('hiddenF5');
+    maskF5.classList.add('hiddenF5');
+  });
+
+  maskF5.addEventListener('click', () => {
+    closeF5.click();
+  });
+// ----------------------------------------------------------------
+  const F6 = document.getElementById('F6');
+  const closeF6 = document.getElementById('closeF6');
+  const modalF6 = document.getElementById('modalF6');
+  const maskF6 = document.getElementById('maskF6');
+
+  F6.addEventListener('click', () => {
+    modalF6.classList.remove('hiddenF6');
+    maskF6.classList.remove('hiddenF6');
+  });
+
+  closeF6.addEventListener('click', () => {
+    modalF6.classList.add('hiddenF6');
+    maskF6.classList.add('hiddenF6');
+  });
+
+  maskF6.addEventListener('click', () => {
+    closeF6.click();
+  });
+// ----------------------------------------------------------------
+  const F7 = document.getElementById('F7');
+  const closeF7 = document.getElementById('closeF7');
+  const modalF7 = document.getElementById('modalF7');
+  const maskF7 = document.getElementById('maskF7');
+
+  F7.addEventListener('click', () => {
+    modalF7.classList.remove('hiddenF7');
+    maskF7.classList.remove('hiddenF7');
+  });
+
+  closeF7.addEventListener('click', () => {
+    modalF7.classList.add('hiddenF7');
+    maskF7.classList.add('hiddenF7');
+  });
+
+  maskF7.addEventListener('click', () => {
+    closeF7.click();
+  });
+// ----------------------------------------------------------------
+  const F8 = document.getElementById('F8');
+  const closeF8 = document.getElementById('closeF8');
+  const modalF8 = document.getElementById('modalF8');
+  const maskF8 = document.getElementById('maskF8');
+
+  F8.addEventListener('click', () => {
+    modalF8.classList.remove('hiddenF8');
+    maskF8.classList.remove('hiddenF8');
+  });
+
+  closeF8.addEventListener('click', () => {
+    modalF8.classList.add('hiddenF8');
+    maskF8.classList.add('hiddenF8');
+  });
+
+  maskF8.addEventListener('click', () => {
+    closeF8.click();
+  });
+// ----------------------------------------------------------------
+  const F9 = document.getElementById('F9');
+  const closeF9 = document.getElementById('closeF9');
+  const modalF9 = document.getElementById('modalF9');
+  const maskF9 = document.getElementById('maskF9');
+
+  F9.addEventListener('click', () => {
+    modalF9.classList.remove('hiddenF9');
+    maskF9.classList.remove('hiddenF9');
+  });
+
+  closeF9.addEventListener('click', () => {
+    modalF9.classList.add('hiddenF9');
+    maskF9.classList.add('hiddenF9');
+  });
+
+  maskF9.addEventListener('click', () => {
+    closeF9.click();
+  });
+  // ----------------------------------------------------------------
+  const F10 = document.getElementById('F10');
+  const closeF10 = document.getElementById('closeF10');
+  const modalF10 = document.getElementById('modalF10');
+  const maskF10 = document.getElementById('maskF10');
+
+  F10.addEventListener('click', () => {
+    modalF10.classList.remove('hiddenF10');
+    maskF10.classList.remove('hiddenF10');
+  });
+
+  closeF10.addEventListener('click', () => {
+    modalF10.classList.add('hiddenF10');
+    maskF10.classList.add('hiddenF10');
+  });
+
+  maskF10.addEventListener('click', () => {
+    closeF10.click();
+  });
+  // ----------------------------------------------------------------
+  const F11 = document.getElementById('F11');
+  const closeF11 = document.getElementById('closeF11');
+  const modalF11 = document.getElementById('modalF11');
+  const maskF11 = document.getElementById('maskF11');
+
+  F11.addEventListener('click', () => {
+    modalF11.classList.remove('hiddenF11');
+    maskF11.classList.remove('hiddenF11');
+  });
+
+  closeF11.addEventListener('click', () => {
+    modalF11.classList.add('hiddenF11');
+    maskF11.classList.add('hiddenF11');
+  });
+
+  maskF11.addEventListener('click', () => {
+    closeF11.click();
+  });
+  // ----------------------------------------------------------------
+  const F12 = document.getElementById('F12');
+  const closeF12 = document.getElementById('closeF12');
+  const modalF12 = document.getElementById('modalF12');
+  const maskF12 = document.getElementById('maskF12');
+
+  F12.addEventListener('click', () => {
+    modalF12.classList.remove('hiddenF12');
+    maskF12.classList.remove('hiddenF12');
+  });
+
+  closeF12.addEventListener('click', () => {
+    modalF12.classList.add('hiddenF12');
+    maskF12.classList.add('hiddenF12');
+  });
+
+  maskF12.addEventListener('click', () => {
+    closeF12.click();
+  });
+  // ----------------------------------------------------------------
+  const F13 = document.getElementById('F13');
+  const closeF13 = document.getElementById('closeF13');
+  const modalF13 = document.getElementById('modalF13');
+  const maskF13 = document.getElementById('maskF13');
+
+  F13.addEventListener('click', () => {
+    modalF13.classList.remove('hiddenF13');
+    maskF13.classList.remove('hiddenF13');
+  });
+
+  closeF13.addEventListener('click', () => {
+    modalF13.classList.add('hiddenF13');
+    maskF13.classList.add('hiddenF13');
+  });
+
+  maskF13.addEventListener('click', () => {
+    closeF13.click();
+  });
+  // ----------------------------------------------------------------
+  const F14 = document.getElementById('F14');
+  const closeF14 = document.getElementById('closeF14');
+  const modalF14 = document.getElementById('modalF14');
+  const maskF14 = document.getElementById('maskF14');
+
+  F14.addEventListener('click', () => {
+    modalF14.classList.remove('hiddenF14');
+    maskF14.classList.remove('hiddenF14');
+  });
+
+  closeF14.addEventListener('click', () => {
+    modalF14.classList.add('hiddenF14');
+    maskF14.classList.add('hiddenF14');
+  });
+
+  maskF14.addEventListener('click', () => {
+    closeF14.click();
+  });
+  // ----------------------------------------------------------------
+  const F15 = document.getElementById('F15');
+  const closeF15 = document.getElementById('closeF15');
+  const modalF15 = document.getElementById('modalF15');
+  const maskF15 = document.getElementById('maskF15');
+
+  F15.addEventListener('click', () => {
+    modalF15.classList.remove('hiddenF15');
+    maskF15.classList.remove('hiddenF15');
+  });
+
+  closeF15.addEventListener('click', () => {
+    modalF15.classList.add('hiddenF15');
+    maskF15.classList.add('hiddenF15');
+  });
+
+  maskF15.addEventListener('click', () => {
+    closeF15.click();
+  });
+}
---
+++ MyWork/MyEx/main.js
@@ -0,0 +1,7 @@
+"use strict"
+{
+    // 終了の操作
+    document.getElementById("close").addEventListener("click", () => {
+      window.close();
+    });
+}
